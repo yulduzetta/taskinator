0eee51f5c64a277011d953feedbe7fac92271ef2
yulduz <yulduz83@gmail.com>
2020-12-17 01:37:10 -0600

    improves user experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,6 +200,10 @@ var dropZoneDragHandler = function (event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute(
+      "style",
+      "background: rgba(68, 233, 255, 0.7); border-style: dashed;"
+    );
   }
 };
 
@@ -218,7 +222,15 @@ var dropTaskHandler = function (event) {
   } else if (statusType === "tasks-completed") {
     statusSelectEl.selectedIndex = 2;
   }
+  dropZoneEl.removeAttribute("style");
   dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function (event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute('style');
+  }
 };
 
 formEl.addEventListener("submit", taskFormHandler);
@@ -227,3 +239,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
